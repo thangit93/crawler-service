d20f86d0c32ea9b637febd5e920f9e880f4a48b3
Thắng Trần Đức <thang.tran@border-z.co.jp>
2023-11-06 10:13:21 +0700

    fix crawler

--- src/commands/crawler.py
+++ src/commands/crawler.py
@@ -2,32 +2,37 @@ import pdfkit
 import requests
 import tempfile
 from bs4 import BeautifulSoup
-from pypdf import PdfMerger
 
 def get_content(url):
     jar = requests.cookies.RequestsCookieJar()
     jar.set('truyenyyid', 'bpffhnjzukasyt2rgmj7oqvslqspc8d6', domain='truyenyy.pro', path='/')
     response = requests.get(url, cookies=jar)
     soup = BeautifulSoup(response.content, "html.parser")
-    contents = soup.find(id='inner_chap_content_1').get_text()
-    return contents
+    contents = str(soup.find(id='inner_chap_content_1'))
+    header = str(soup.find(class_='heading-font mt-2'))
+    return contents, header
 
 def main():
     # Test
     # https://truyenyy.pro/truyen/bat-dau-danh-dau-hoang-co-thanh-the-ban-dich/chuong-1.html
     with tempfile.TemporaryDirectory() as tmpdir:
-        merger = PdfMerger()
-        for i in range(1, 50):
+        full_contents = "<h1 style='text-align: center'>Bắt Đầu Đánh Dấu Hoang Cổ Thánh Thể. ( Bản dịch. )</h1>"
+        file_path = f"chap.pdf"
+        # file_path = f"{tmpdir}/Crawler.pdf"
+        for i in range(1, 200):
             url = f"https://truyenyy.pro/truyen/bat-dau-danh-dau-hoang-co-thanh-the-ban-dich/chuong-{i}.html"
-            contents = get_content(url)
-            file_path = f"{tmpdir}/chap_{i}.pdf"
-            # TODO: Error format
-            pdfkit.from_string(contents, file_path)
-            merger.append(file_path)
-        merger.write("result.pdf")
-        merger.close()
+            try:
+                contents, header = get_content(url)
+            except:
+                print("Error or page not exit")
+                continue
+            options = {
+                'encoding': "UTF-8",
+            }
+            if contents is not None:
+                full_contents += header + contents + "<br>"
+        pdfkit.from_string(full_contents, file_path, options=options)
         
-
 
 if __name__ == "__main__":
     main()
